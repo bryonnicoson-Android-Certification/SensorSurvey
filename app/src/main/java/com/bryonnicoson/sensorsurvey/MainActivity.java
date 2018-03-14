38952057d62e701566197e85af79a75b86380914
Bryon Nicoson <bryon.nicoson@gmail.com>
2018-03-14 17:30:22 -0500

    light & proximity sensors

--- app/src/main/java/com/bryonnicoson/sensorsurvey/MainActivity.java
+++ app/src/main/java/com/bryonnicoson/sensorsurvey/MainActivity.java
@@ -2,31 +2,95 @@ package com.bryonnicoson.sensorsurvey;
 
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager mSensorManager;
+
+    // light and proximity sensors
+    private Sensor mSensorLight;
+    private Sensor mSensorProximity;
+
+    // textviews to display sensor values
+    private TextView mTextSensorLight;
+    private TextView mTextSensorProximity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // views
+        mTextSensorLight = findViewById(R.id.label_light);
+        mTextSensorProximity = findViewById(R.id.label_proximity);
+
+        // error message
+        String error = getResources().getString(R.string.error_no_sensor);
+
+        // sensors
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        List<Sensor> sensorList = mSensorManager.getSensorList(Sensor.TYPE_ALL);
 
-        String sensorText = "";
-        for (Sensor sensor : sensorList) {
-            sensorText = sensorText.concat(sensor.getName()).concat(System.getProperty("line.separator"));
+
+        mSensorLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        mSensorProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+        if (mSensorLight == null) mTextSensorLight.setText(error);
+        if (mSensorProximity == null) mTextSensorProximity.setText(error);
+    }
+
+    /**
+     *  Use onStart/onStop to register/unregister sensor listeners
+     *  onCreate = wasting resources while app is not visible
+     *  onResume/onPause = not listening while app is visible in multi-window mode (API 24+)
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (mSensorProximity != null)
+            mSensorManager.registerListener(this, mSensorProximity,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        if (mSensorLight != null)
+            mSensorManager.registerListener(this, mSensorLight,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mSensorManager.unregisterListener(this);  // single call unregisters 'm all
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        int sensorType = event.sensor.getType();
+        float currentValue = event.values[0];  // light & proximity have just the one value
+
+        switch (sensorType) {
+            case Sensor.TYPE_LIGHT:
+                mTextSensorLight.setText(getResources().getString(
+                        R.string.label_light, currentValue));
+                break;
+            case Sensor.TYPE_PROXIMITY:
+                mTextSensorProximity.setText(getResources().getString(
+                        R.string.label_proximity, currentValue));
+                break;
+            default:
+                // do nothing
         }
+    }
 
-        TextView sensorTextView = findViewById(R.id.sensor_list);
-        sensorTextView.setText(sensorText);
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
