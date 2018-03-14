a5987fbc8e0fd02b6113173440765e9abe26f7b1
Bryon Nicoson <bryon.nicoson@gmail.com>
2018-03-14 16:43:43 -0500

    sensor list

--- app/src/main/java/com/bryonnicoson/sensorsurvey/MainActivity.java
+++ app/src/main/java/com/bryonnicoson/sensorsurvey/MainActivity.java
@@ -1,13 +1,32 @@
 package com.bryonnicoson.sensorsurvey;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SensorManager mSensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> sensorList = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        String sensorText = "";
+        for (Sensor sensor : sensorList) {
+            sensorText = sensorText.concat(sensor.getName()).concat(System.getProperty("line.separator"));
+        }
+
+        TextView sensorTextView = findViewById(R.id.sensor_list);
+        sensorTextView.setText(sensorText);
     }
 }
